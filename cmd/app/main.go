4aba6c438c4888eac364b78694c06f3396254974
NanReven <nanreven@gmail.com>
2025-06-09 12:48:42 +0300

    setup main.go file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,17 +3,49 @@ package main
 import (
 	"QuizService/internal/handler"
 	"QuizService/internal/infrastructure"
+	"QuizService/internal/repository"
 	"QuizService/internal/usecase"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	//repository := repository.NewRepository()
-	usecase := usecase.NewUsecase()
+	if err := initConfig(); err != nil {
+		logrus.Fatal("failed to parse config", err.Error())
+	}
+
+	if err := godotenv.Load(".env"); err != nil {
+		logrus.Fatal("failed to load env file", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(&repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		logrus.Fatal("failed to connect to db", err.Error())
+	}
+
+	repository := repository.NewRepository(db)
+	usecase := usecase.NewUsecase(repository)
 	handler := handler.NewHandler(usecase)
 
 	server := new(infrastructure.Server)
 	log.Fatal(server.Run("8000", handler.InitRouter()))
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
