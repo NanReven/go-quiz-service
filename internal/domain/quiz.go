75c8186454783439e9e0d8064b2be3e0e3f2e0bf
NanReven <nanreven@gmail.com>
2025-06-17 19:49:06 +0300

    update quiz entity

--- internal/domain/quiz.go
+++ internal/domain/quiz.go
@@ -1,19 +1,36 @@
 package domain
 
-import "time"
+import (
+	"database/sql"
+	"errors"
+	"time"
+)
+
+var (
+	ErrQuizNotFound       = errors.New("quiz not found")
+	ErrInvalidUpdateInput = errors.New("invalid input")
+	ErrQuizAlreadyDeleted = errors.New("quiz is deleted already")
+)
 
 type Quiz struct {
-	ID          int       `json:"id" db:"id"`
-	AuthorID    int       `json:"author_id" db:"author_id"`
-	Title       string    `json:"title" binding:"required" db:"title"`
-	Description string    `json:"description" binding:"required" db:"description"`
-	CreatedAt   time.Time `json:"created_at" db:"created_at"`
-	DeletedAt   time.Time `json:"deleted_at" db:"deleted_at"`
-	IsActive    bool      `json:"is_active" db:"is_active"`
+	ID          int          `json:"id" db:"id"`
+	AuthorID    int          `json:"author_id" db:"author_id"`
+	Title       string       `json:"title" db:"title"`
+	Description string       `json:"description" db:"description"`
+	CreatedAt   time.Time    `json:"created_at" db:"created_at"`
+	DeletedAt   sql.NullTime `json:"deleted_at" db:"deleted_at"`
+	IsActive    bool         `json:"is_active" db:"is_active"`
 }
 
-type CreateQuiz struct {
-	AuthorID    int    `json:"author_id" db:"author_id"`
+type CreateQuizInput struct {
+	AuthorID    int    `db:"author_id"`
 	Title       string `json:"title" binding:"required" db:"title"`
 	Description string `json:"description" binding:"required" db:"description"`
+}
+
+type UpdateQuizInput struct {
+	QuizID      int    `db:"id"`
+	AuthorID    int    `db:"author_id"`
+	Title       string `json:"title" db:"title"`
+	Description string `json:"description" db:"description"`
 }
